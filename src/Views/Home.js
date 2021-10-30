315c2867a2eb6ac10ebf64d26e5b616e405d6364
Alan Fidelino <55466923+afimonsido@users.noreply.github.com>
2021-10-30 23:07:48 +1100

    Add date of submission

--- src/Views/Home.js
+++ src/Views/Home.js
@@ -3,6 +3,7 @@ const Home = () => {
         <div className="page-title content">
         <h2>Technical Account Manager(TAM) Candidate Deliverables</h2>
         <h3>This site will contain the answers to the challenges from TAM Candidate - Alan Fidelino</h3>
+        <p>Date of submission: 1 November 2021</p>
       </div>
     );
 }
