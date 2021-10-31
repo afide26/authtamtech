bde182298da3d35ce7246f405cce3545bdd0c15c
Alan Fidelino <55466923+afimonsido@users.noreply.github.com>
2021-10-31 19:48:25 +1100

    Implement logout and login

--- src/Views/Navbar.js
+++ src/Views/Navbar.js
@@ -1,14 +1,23 @@
 import {Link} from 'react-router-dom'
-
+import { useAuth0 } from '@auth0/auth0-react';
+import LoginButton from '../Components/LoginButton';
+import LogoutButton from '../Components/LogoutButton';
 const Navbar = () => {
-    return (
+    const {isAuthenticated} = useAuth0();
+    
+    return(
         <nav className="navbar">
             <Link to="/" className="nav-title-link"><div className="nav-title">Auth0 TAM Test</div></Link>
-            <div className="links">
+            {isAuthenticated && 
+                (<div className="links">
                 <Link to="/" exact>Home</Link>
                 <Link to="/challengeone">Challenge One</Link>
                 <Link to="/challengetwo">Challenge Two</Link>
                 <Link to="/challengethree">Challenge Three</Link>
+                <LogoutButton/>
+            </div>)}
+            <div className="links">
+                <LoginButton/>
             </div>
         </nav>
     );
