8aa568edd1ec8e6dda1efeb4cfa845943b2e57bf
Alan Fidelino <55466923+afimonsido@users.noreply.github.com>
2021-10-31 20:31:38 +1100

    Add styling to profile

--- src/Components/Profile.js
+++ src/Components/Profile.js
@@ -1,14 +1,15 @@
 import { useAuth0 } from "@auth0/auth0-react";
 import JSONPretty from 'react-json-pretty'
 const Profile = () => {
-    const {user} = useAuth0();
+    const {user,isAuthenticated} = useAuth0();
 
     return (
-        <div className="profile">
-            <h3 className="profile">
+       isAuthenticated && <div className="profile">
+            <h3>
                 <span><img src={user.picture} alt={user.nickname} /></span>
                 Welcome {user.name}
             </h3>
+            <p>{user.name}'s details from Google OAuth2</p>
             <code>
                 <JSONPretty data={user}/>
             </code>
--- src/Views/Navbar.js
+++ src/Views/Navbar.js
@@ -8,17 +8,15 @@ const Navbar = () => {
     return(
         <nav className="navbar">
             <Link to="/" className="nav-title-link"><div className="nav-title">Auth0 TAM Test</div></Link>
-            {isAuthenticated && 
-                (<div className="links">
+ 
+            <div className="links">
                 <Link to="/" exact>Home</Link>
                 <Link to="/challengeone">Challenge One</Link>
                 <Link to="/challengetwo">Challenge Two</Link>
                 <Link to="/challengethree">Challenge Three</Link>
-                <LogoutButton/>
-            </div>)}
-            {!isAuthenticated && <div className="links">
-                <LoginButton/>
-            </div>}
+                {!isAuthenticated && <LoginButton/>}
+                {isAuthenticated && <LogoutButton/>}
+            </div>
         </nav>
     );
 }
