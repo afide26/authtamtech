d69adff731d498b544f169ee85e72f0af5599634
Alan Fidelino <55466923+afimonsido@users.noreply.github.com>
2021-11-01 20:26:10 +1100

    Fix welcome message

--- src/Views/Welcome.js
+++ src/Views/Welcome.js
@@ -7,13 +7,8 @@ const Welcome = () => {
     const {isAuthenticated, user} = useAuth0()
     return (
         <div className="page-title content profile">
-
-        {!isAuthenticated && <h2>
-          <span><img src={image} alt="Cruise0" /></span> 
-        </h2>}
         <h2>
           <span><img src={image} alt="Cruise0" /></span>     
-          Welcome {user.name} your next advanture awaits you.    
         </h2>
         <h3>Explore the possibilities of traveling around the world with luxurious and affordable packages from Cruise0.</h3>
         <p>Use the choices below to book your package</p>
